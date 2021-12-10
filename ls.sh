c656afa7c8f7a7e3e2a45b540c02115b77cf6c06
dk949 <56653556+dk949@users.noreply.github.com>
2021-12-10 11:45:40 +0000

    udpated l alias

--- ls.sh
+++ ls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # ls aliases
-alias l='ls -CFv --color=auto'
+alias l='ls -lv --color=auto'
 alias la='ls -Av --color=auto'
 alias ll='ls -AlFhv --color=auto'
 alias ls='ls -v --color=auto'
