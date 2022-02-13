630fc2d39f056e095377e9536000786dfe4b9d11
dk949 <56653556+dk949@users.noreply.github.com>
2022-02-13 14:53:31 +0000

    added gap and gds. added -v to gc and gca

--- git.sh
+++ git.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 # git aliases
-alias dk949='echo https://github.com/dk949'
+export dk949='git@github.com:dk949'
 
 alias ga='git add'
-alias gc='git commit'
-alias gca='git commit --amend'
+alias gap='git add -p'
+alias gc='git commit -v'
+alias gca='git commit --amend -v'
 alias gcd='git switch'
 alias gcdd='git switch -c'
 alias gcm='git commit -m'
 alias gcms='git commit -m "Sync"'
 alias gd='git diff'
+alias gds='git diff --staged'
 alias gi='git ignore'
 alias gp='git push'
 alias gpt='git push --tags'
