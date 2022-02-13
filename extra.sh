e17e82172d484a10b0e074c706ebe4d4bacfcfd8
dk949 <56653556+dk949@users.noreply.github.com>
2022-02-13 10:02:44 +0000

    added function to quickly switch compilers

--- extra.sh
+++ extra.sh
@@ -9,3 +9,26 @@ clearfile() {
 alias reboot='sudo reboot'
 
 alias o='xdg-open'
+
+comp() {
+    if [ $# -ne 1 ]; then
+        echo "Usage: comp [COMPILER]"
+    fi
+
+    case $1 in
+        gcc|g++)
+            export CC=gcc CXX=g++
+            return 0
+            ;;
+
+        clang|clang++)
+            export CC=clang CXX=clang++
+            return 0
+            ;;
+        *)
+            echo "Unsupported compiler $1"
+            return 1
+            ;;
+    esac
+
+}
