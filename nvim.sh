e509581ee58e5c0df47fd2a5e93c692e4e2f8bcf
dk949 <56653556+dk949@users.noreply.github.com>
2022-02-13 10:03:33 +0000

    added function to open all files matched by ripgrep in a new vim tab

--- nvim.sh
+++ nvim.sh
@@ -7,7 +7,12 @@ alias ses='nvim -S Session.vim'
 alias vimwiki='nvim +VimwikiIndex'
 
 ncpp() {
-    ___FILE_NAME="$(echo "$1" | cut -d'.' -f 1)"
-    nvim "${___FILE_NAME}.hpp" "${___FILE_NAME}.cpp" -O
-    unset ___FILE_NAME
+    FILE_NAME="$(echo "$1" | cut -d'.' -f 1)"
+    nvim "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
+    unset FILE_NAME
+}
+
+
+nre() {
+    nvim $(rg $1 -l) -p
 }
