6c4d427549b35d93a4c6ee4793c00e15210e9136
dk949 <56653556+dk949@users.noreply.github.com>
2022-02-13 10:03:45 +0000

    swtched ls to exa

--- ls.sh
+++ ls.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+
+export EXA_COMMON_FLAGS="--color-scale --color=auto --no-icons --sort=Name --git"
+#export EXA_COLORS="da=38;5;074" # light blue
+#export EXA_COLORS="da=38;5;102" # dark gray
+export EXA_COLORS="da=38;5;145" # lighter gray (xterm grey69)
+
 # ls aliases
-alias l='ls -lv --color=auto'
-alias la='ls -Av --color=auto'
-alias ll='ls -AlFhv --color=auto'
-alias ls='ls -v --color=auto'
+alias l="exa -l $EXA_COMMON_FLAGS"
+alias la="exa -a $EXA_COMMON_FLAGS"
+alias lg="exa --git --git-ignore -al $EXA_COMMON_FLAGS"
+alias ll="exa -al $EXA_COMMON_FLAGS"
+alias ls="exa $EXA_COMMON_FLAGS"
