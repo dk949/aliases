1cbb7cfeac10c682d9d9ff63903e8d84c80d7920
dk949 <56653556+dk949@users.noreply.github.com>
2022-06-30 20:28:22 +0300

    added 'save' and 'goback'

--- cd.sh
+++ cd.sh
@@ -2,9 +2,34 @@
 cdd(){
     if [ $# -ne 1 ]; then
         echo "Usage: $(basename "$0") DIR"
-        exit 1
+        return 1
     fi
 
     mkdir -p "$1"
-    cd "$1"
+    cd "$1" || return 1
+}
+
+
+
+save() {
+    if [ $# -gt 1 ]; then
+        echo "Usage: $(basename "$0") [TAG]"
+        return 1
+    fi
+
+    echo "$PWD" > "/tmp/dirsave_$1"
+}
+
+goback() {
+    if [ $# -gt 1 ]; then
+        echo "Usage: $(basename "$0") [TAG]"
+        return 1
+    fi
+
+    if [ ! -f "/tmp/dirsave_$1" ]; then
+        echo "Tag file /tmp/dirsave_$1 does not exist"
+        return 1
+    fi
+
+    cd "$(cat "/tmp/dirsave_$1")" || return 1
 }
