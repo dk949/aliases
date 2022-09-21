dec87c8a4e428d02d615f1bd9a571458097b30be
dk949 <56653556+dk949@users.noreply.github.com>
2022-09-21 19:51:56 +0100

    handle ldc in dvm

--- dvm.sh
+++ dvm.sh
@@ -2,12 +2,27 @@
 
 dvm(){
     ___CMD="install.sh --path $HOME/.local/bin/dlang"
+    doActivate(){
+        ___PATH="$(eval "$___CMD get-path $1")"
+        # shellcheck disable=SC1090 disable=SC1091
+        if [ "$1" = "dmd" ] || [ -z "$1" ]; then
+            . "$(dirname "$___PATH")/../../activate"
+            return
+        elif  [ "$1" = "ldc" ] || [ "$1" = "gdc" ]; then
+            . "$(dirname "$___PATH")/../activate"
+            return
+        else # TODO: handle gdc
+            echo "Unknown compiler " "$1"
+            return 1
+        fi
+
+    }
 
     case "$1" in
         activate)
             shift
-            # shellcheck disable=SC1090 disable=SC1091
-            . "$(dirname "$(eval "$___CMD get-path $1")")/../../activate"
+            doActivate "$1"
+            return
             ;;
         deactivate)
             deactivate
