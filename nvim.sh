5483d0fe0364e770d9c842b95f16b83dd51386a2
dk949 <56653556+dk949@users.noreply.github.com>
2023-02-11 15:38:41 +0000

    if not in nvim ternimal spwan normal nvim, else sue nvr

--- nvim.sh
+++ nvim.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 # nvim aliases
-alias n='nvim'
-alias nt='nvim .'
+
+n() {
+    if [ -n "$NVIM" ]; then
+        nvr --servername "$NVIM" "$@"
+    else
+        nvim "$@"
+    fi
+}
+
+
+alias nt="n +'term' -c 'startinsert'"
+alias nb='n -s .'
 alias ns='sudo nvim'
 alias ses='nvim -S Session.vim'
-alias vimwiki='nvim +VimwikiIndex'
+alias vimwiki='n +VimwikiIndex'
+alias vim='nvim -u NONE'
 
 ncpp() {
     FILE_NAME="$(echo "$1" | cut -d'.' -f 1)"
-    nvim "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
+    nvr -s "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
     unset FILE_NAME
 }
 
 
 nre() {
-    nvim $(rg $1 -l)
+    nvr -s $(rg $1 -l)
 }
