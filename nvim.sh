28f121882f8c46618fd34b75d5d4311e06255722
dk949 <56653556+dk949@users.noreply.github.com>
2023-04-18 16:43:15 +0100

    trying to fix bracket mismatch between rg and nvim

--- nvim.sh
+++ nvim.sh
@@ -11,19 +11,26 @@ n() {
 
 
 alias nt="n +'term' -c 'startinsert'"
-alias nb='n -s .'
-alias ns='sudo nvim'
+alias nb="n +':NvimTreeOpen'"
+alias ns='sudo nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
 alias ses='nvim -S Session.vim'
 alias vimwiki='n +VimwikiIndex'
 alias vim='nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
 
 ncpp() {
     FILE_NAME="$(echo "$1" | cut -d'.' -f 1)"
-    nvr -s "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
+    n "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
     unset FILE_NAME
 }
 
 
 nre() {
-    nvr -s $(rg $1 -l)
+    # Get two unique strings
+    ___F=$(echo "$1" | md5sum | cut -d' ' -f 1)
+    ___R=$(echo "$1" | rev | md5sum | cut -d' ' -f 1)
+    # replace ( -> \(, ) -> \), \( -> ( and \) -> )
+    n $(rg "$1" -l) +"silent /$(echo "$1" | sed "s/\\\\(/$___F/g;s/\\\\)/$___R/g;s/(/\\\\(/g;s/)/\\\\)/g;s/$___F/(/g;s/$___R/)/g;")"
+    unset ___F
+    unset ___R
 }
+
