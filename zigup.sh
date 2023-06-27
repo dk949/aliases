d1be2c629ff45fc48de7ebded8454a2dd8490842
dk949 <56653556+dk949@users.noreply.github.com>
2023-06-27 20:37:14 +0100

    added zigup

---
+++ zigup.sh
@@ -0,0 +1,15 @@
+
+zigup() {
+    for VER in "$@"; do :; done
+    /usr/bin/zigup --install-dir ~/.cache/zigup --path-link ~/.local/bin/zig "$@" || return
+    if [ -d "$XDG_CACHE_HOME/zigup/zls" ]; then
+        if [ -f "$XDG_CACHE_HOME/zigup/zls/zls-$VER" ]; then
+            ln -sf "$XDG_CACHE_HOME/zigup/zls/zls-$VER" "$XDG_DATA_HOME/../bin/zls"
+        else
+            echo "WARN: no zls built for version $VER"
+        fi
+    else
+        echo "WARN: no zls versions found"
+    fi
+    unset VER
+}
