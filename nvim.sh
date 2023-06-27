3a47415c6355684bb388a029f0e75d015af0477c
dk949 <56653556+dk949@users.noreply.github.com>
2023-06-27 20:35:55 +0100

    load a different config on root and regular user

--- nvim.sh
+++ nvim.sh
@@ -1,36 +1,42 @@
 #!/bin/sh
 # nvim aliases
 
-n() {
-    if [ -n "$NVIM" ]; then
-        nvr --servername "$NVIM" "$@"
-    else
-        nvim "$@"
-    fi
-}
+if [ "$(id -u)" = "0" ]; then
+    n() {
+        nvim -u "$XDG_CONFIG_HOME/vim/vimrc" --noplugin "$@"
+    }
+else
+    n() {
+        if [ -n "$NVIM" ]; then
+            nvr --servername "$NVIM" "$@"
+        else
+            nvim "$@"
+        fi
+    }
 
 
-alias nt="n +'term' -c 'startinsert'"
-alias nb="n +':NvimTreeOpen'"
-alias ns='sudo nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
-alias ses='nvim -S Session.vim'
-alias vimwiki='n +VimwikiIndex'
-alias vim='nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
+    alias nt="n +'term' -c 'startinsert'"
+    alias nb="n +':NvimTreeOpen'"
+    alias ns='sudo nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
+    alias ses='nvim -S Session.vim'
+    alias vimwiki='n +VimwikiIndex'
+    alias vim='nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
 
-ncpp() {
-    FILE_NAME="$(echo "$1" | cut -d'.' -f 1)"
-    n "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
-    unset FILE_NAME
-}
+    ncpp() {
+        FILE_NAME="$(echo "$1" | cut -d'.' -f 1)"
+        n "${FILE_NAME}.hpp" "${FILE_NAME}.cpp" -O
+        unset FILE_NAME
+    }
 
 
-nre() {
-    # Get two unique strings
-    ___F=$(echo "$1" | md5sum | cut -d' ' -f 1)
-    ___R=$(echo "$1" | rev | md5sum | cut -d' ' -f 1)
-    # replace ( -> \(, ) -> \), \( -> ( and \) -> )
-    n $(rg "$1" -l) +"silent /$(echo "$1" | sed "s/\\\\(/$___F/g;s/\\\\)/$___R/g;s/(/\\\\(/g;s/)/\\\\)/g;s/$___F/(/g;s/$___R/)/g;")"
-    unset ___F
-    unset ___R
-}
+    nre() {
+        # Get two unique strings
+        ___F=$(echo "$1" | md5sum | cut -d' ' -f 1)
+        ___R=$(echo "$1" | rev | md5sum | cut -d' ' -f 1)
+        # replace ( -> \(, ) -> \), \( -> ( and \) -> )
+        n $(rg "$1" -l) +"silent /$(echo "$1" | sed "s/\\\\(/$___F/g;s/\\\\)/$___R/g;s/(/\\\\(/g;s/)/\\\\)/g;s/$___F/(/g;s/$___R/)/g;")"
+        unset ___F
+        unset ___R
+    }
 
+fi
