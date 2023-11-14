5db288b228192672d1cc5ca92c6ade23329f7fe0
dk949 <56653556+dk949@users.noreply.github.com>
2023-11-14 11:39:10 +0000

    migrating to use eza settings

--- ls.sh
+++ ls.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-export EXA_COMMON_FLAGS="--color-scale --color=auto --icons --sort=Name --git"
+export EZA_COMMON_FLAGS="--color-scale size --color=auto --icons --sort=Name --git"
 
-#export EXA_COLORS="da=38;5;074" # light blue
-#export EXA_COLORS="da=38;5;102" # dark gray
-export EXA_COLORS="da=38;5;145" # lighter gray (xterm grey69)
+#export EZA_COLORS="da=38;5;074" # light blue
+#export EZA_COLORS="da=38;5;102" # dark gray
+#export EZA_COLORS="da=38;5;145" # lighter gray (xterm grey69)
+export EZA_COLORS="da=38;5;145" # lighter gray (xterm grey69)
 
 # ls aliases
-alias l="exa -l $EXA_COMMON_FLAGS"
-alias la="exa -a $EXA_COMMON_FLAGS"
-alias lg="exa --git --git-ignore -al $EXA_COMMON_FLAGS"
-alias ll="exa -al $EXA_COMMON_FLAGS"
-alias ls="exa $EXA_COMMON_FLAGS"
+alias l="exa -l $EZA_COMMON_FLAGS"
+alias la="exa -a $EZA_COMMON_FLAGS"
+alias lg="exa --git --git-ignore -al $EZA_COMMON_FLAGS"
+alias ll="exa -al $EZA_COMMON_FLAGS"
+alias ls="exa $EZA_COMMON_FLAGS"
