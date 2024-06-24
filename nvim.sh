75310cb255d2870d9942e06305892d8aa0bc2a59
dk949 <56653556+dk949@users.noreply.github.com>
2024-06-24 16:10:56 +0100

    nvim

--- nvim.sh
+++ nvim.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 # nvim aliases
 
-alias nvim='nvim $([ -n "$NVIM" ] && echo "--server $NVIM") --remote'
+# shellcheck disable=2016,2139
+# alias nvim='nvim $([ -n "$NVIM" ] && echo "--server $NVIM") --remote'
+export GIT_EDITOR='nvim $([ -n "$NVIM" ] && echo "--server $NVIM --remote" || echo "--remote")'
+alias nvim="$GIT_EDITOR"
 
 if [ "$(id -u)" = "0" ]; then
     n() { \nvim -u "$XDG_CONFIG_HOME/vim/vimrc" --noplugin "$@"; }
 else
-    n() { nvim; }
+    n() { nvim "$@"; }
 
     alias nt="n +'term' -c 'startinsert'"
     alias nb="n +':NvimTreeOpen'"
     alias ns='sudo nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
-    alias ses='nvim -S Session.vim'
+    alias ses='n -S Session.vim'
     alias vimwiki='n +VimwikiIndex'
     alias vim='nvim -u $XDG_CONFIG_HOME/vim/vimrc --noplugin'
 
@@ -27,7 +30,17 @@ else
         ___F=$(echo "$1" | md5sum | cut -d' ' -f 1)
         ___R=$(echo "$1" | rev | md5sum | cut -d' ' -f 1)
         # replace ( -> \(, ) -> \), \( -> ( and \) -> )
-        n $(rg "$1" -l) +"silent /$(echo "$1" | sed "s/\\\\(/$___F/g;s/\\\\)/$___R/g;s/(/\\\\(/g;s/)/\\\\)/g;s/$___F/(/g;s/$___R/)/g;")"
+
+        old_ifs=$IFS
+
+        IFS=$'\n'
+        #shellcheck disable=SC2207 # no
+        files=($(rg "$1" -l))
+
+        n "${files[@]}" +"silent /$(echo "$1" | sed "s/\\\\(/$___F/g;s/\\\\)/$___R/g;s/(/\\\\(/g;s/)/\\\\)/g;s/$___F/(/g;s/$___R/)/g;")"
+
+        IFS=$old_ifs
+        unset old_ifs
         unset ___F
         unset ___R
     }
