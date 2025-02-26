9c47cbbb50404b223bd59637efead5a0e3f13210
dk949 <56653556+dk949@users.noreply.github.com>
2025-02-26 10:52:42 +0000

    no more rsync

--- rsync.sh
+++ rsync.sh
@@ -1,10 +1,1 @@
-rsync___() {
-    if [ -f ./.rsyncignore ]; then
-        \rsync  $(awk '{print "--exclude " $0}' .rsyncignore) "$@"
-    else
-        \rsync "$@"
-    fi
-}
 
-
-alias rsync='rsync___'
