f8a769d27edd4c42edef1c373b707005199948cf
dk949 <56653556+dk949@users.noreply.github.com>
2025-02-26 10:51:56 +0000

    added completion for the comp command

--- complete.sh
+++ complete.sh
@@ -3,3 +3,10 @@
 # completions
 #shell="$(readlink -f /proc/$$/exe | rev | cut -d'/' -f 1 | rev)"
 compdef _path_commands run
+
+_comp() {
+    local -a compilers
+    compilers=(gcc clang default)
+    _describe 'compilers' compilers
+}
+compdef _comp comp
