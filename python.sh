9dc458131b94891958cce4eeac6b10ce6a110712
dk949 <56653556+dk949@users.noreply.github.com>
2025-07-20 14:39:26 +0100

    extract venv detection logic into a function

--- python.sh
+++ python.sh
@@ -25,36 +25,51 @@ venv() {
 
 # Checks if the current directory has a venv. If it does, it gets activated. If no then a message is printed
 
+___get_venv_activate_path() {
+    local activate_path
+    if [ -n "$1" ]; then
+        if [ -d "$1" ] && [ -f "$1/bin/activate" ]; then
+            activate_path=$1/bin/activate
+        else
+            echo "($1) is not a valid virtual environment" 1>&2
+            return 2
+        fi
+    else
+        if ! activate_path=$(find . -wholename '*/bin/activate' -type f | grep .); then
+            echo "No virtual environment found"  1>&2
+            return 3
+        fi
+    fi
+    local print_name=$(sed -E 's|^(\./)?([^/]*).*$|(\2)|g' <<< "$activate_path")
+    if [ "$(wc -l <<< "$activate_path")" -ne 1 ]; then
+        printf "Multiple candidate virtual environments found:\n\n%s\n\nSelect one explicitly\n" "$print_name"  1>&2
+        return 4
+    fi
+    
+    if [ -z "$activate_path" ]; then
+        echo "Internal error, could not find virtual environment" 1>&2
+        return 255
+    fi
+    echo "$activate_path":"$print_name"
+}
+
 activate() {
     if [ $# -gt 1 ]; then
         echo "Usage: activate [VENV_NAME]"
         return 1
     fi
 
-    local activate_path
-    if [ -n "$1" ]; then
-        if [ -d "$1" ] && [ -f "$1/bin/activate" ]; then
-            activate_path=$1/bin/activate
-        else
-            echo "($1) is not a valid virtual environment"
-            return 1
-        fi
-    else
-        if ! activate_path=$(find . -wholename '*/bin/activate' -type f | grep .); then
-            echo "No virtual environment found"
-            return 1
-        fi
+    local path_print_name
+    local path_print_code
+    path_print_name=$(___get_venv_activate_path $1)
+    path_print_code=$?
+    if [ "$path_print_code" -ne 0 ]; then
+        return "$path_print_code"
     fi
-    local print_name=$(sed -E 's|^(\./)?([^/]*).*$|(\2)|g' <<< "$activate_path")
-    if [ "$(wc -l <<< "$activate_path")" -ne 1 ]; then
-        printf "Multiple candidate virtual environments found:\n\n%s\n\nSelect one explicitly\n" "$print_name"
-        return 1
-    fi
-    
-    if [ -z "$activate_path" ]; then
-        echo "Internal error, could not find virtual environment"
-        return 2
-    fi
+
+    activate_path=$(cut -d: -f1 <<< "$path_print_name")
+    print_name=$(cut -d: -f2 <<< "$path_print_name")
+
     echo "Activating $print_name virtual environment"
     . "$activate_path"
 
